90cb66b76d1755a37439ec9e58230b113f3e9b85
pashameed-art <pashameed@gmail.com>
2026-09-08 21:17:50 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE="iob-upi-qr-v1";
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"])));self.skipWaiting()});
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
